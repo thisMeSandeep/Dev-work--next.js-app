7c1392eb2cbe3615e57b69b5e4ebc232292d2049
Sandeep Singh <snayal50@gmail.com>
2025-08-26 19:35:25 +0530

    client pages

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,11 +1,7 @@
-import { auth } from "@/auth"
+
 
 
 const Home = async () => {
-  const session = await auth()
-
-  console.log("session data:" , session)
-
   return (
     <div>Home</div>
   )
---
+++ app/client/jobs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostedJobs = () => {
+  return (
+    <div>PostedJobs</div>
+  )
+}
+
+export default PostedJobs
