51563b990a1d5506fd9aea545d5aef67e23e3f27
Sandeep Singh <snayal50@gmail.com>
2025-09-15 00:04:08 +0530

    fix prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@/generated/prisma"; 
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// Avoids type conflicts when Next.js does fast reloads in dev
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalThis.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query"] : [],
   });
 
-if (process.env.NODE_ENV === "development") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
